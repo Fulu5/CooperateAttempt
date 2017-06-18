cec059b65dcec608ec5f096803662ff0e5c09b28
曹冠雄 <1120643432@qq.com>
2017-06-18 13:33:12 +0800

    Init xcode proj with ignore file

---
+++ CooperateAttempt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CooperateAttempt
+//
+//  Created by 曹曹 on 2017/6/18.
+//  Copyright © 2017年 曹曹. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CooperateAttempt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CooperateAttempt
+//
+//  Created by 曹曹 on 2017/6/18.
+//  Copyright © 2017年 曹曹. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
